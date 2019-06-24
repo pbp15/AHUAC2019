630e03bcceeca3a5dbd9527b8c2e2f6e2b6e9e88
PIERO <gatopbp123@gmail.com>
2019-06-24 04:44:47 -0500

    TERMINADO LOS LOGEOS Y AUTHENTICACION DE USUARIOS CON SUS ROLES

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -1,60 +1,122 @@
     @extends('principal')
     @section('contenido')
-        <template v-if="menu==0">
-        <h1>Contenido   General </h1>
-        </template>
 
-        <template v-if="menu==1">
-            <oficina></oficina>
-        </template>
 
-        <template v-if="menu==2">
-            <h1>Contenido del menú 2</h1>
-        </template>
 
-        <template v-if="menu==3">
-            <expediente></expediente>
-        </template>
+    @if(Auth::check())
+          @if(Auth::user()->idrol == 1)               
+          
+                <template v-if="menu==0">
+                <h1>Escritorio </h1>
+                </template>
 
-        <template v-if="menu==4">
-            <solicitante></solicitante>
-        </template>
+                <template v-if="menu==1">
+                    <oficina></oficina>
+                </template>
 
-        <template v-if="menu==5">
-            <regexpediente></regexpediente>
-        </template>
+                <template v-if="menu==2">
+                    <h1>Contenido del menú 2</h1>
+                </template>
 
-  
+                <template v-if="menu==3">
+                    <expediente></expediente>
+                </template>
 
-        <template v-if="menu==6">
-            <h1>Contenido del menú 6</h1>
-        </template>
-        <template v-if="menu==13">
-            <persona></persona>
-        </template>
+                <template v-if="menu==4">
+                    <solicitante></solicitante>
+                </template>
 
-        <template v-if="menu==7">
-            <user></user>
-        </template>
+                <template v-if="menu==5">
+                    <regexpediente></regexpediente>
+                </template>
 
-        <template v-if="menu==8">
-            <rol></rol>
-        </template>
-
-        <template v-if="menu==9">
-            <h1>Contenido del menú 9</h1>
-        </template>
-
-        <template v-if="menu==10">
-            <h1>Contenido del menú 10</h1>
-        </template>
-
-        <template v-if="menu==11">
-            <h1>Contenido del menú 11</h1>
-        </template>
-
-        <template v-if="menu==12">
-            <h1>Contenido del menú 12</h1>
-        </template>
         
+
+                <template v-if="menu==6">
+                    <h1>Contenido del menú 6</h1>
+                </template>
+                <template v-if="menu==13">
+                    <persona></persona>
+                </template>
+
+                <template v-if="menu==7">
+                    <user></user>
+                </template>
+
+                <template v-if="menu==8">
+                    <rol></rol>
+                </template>
+
+                <template v-if="menu==9">
+                    <h1>Reporte Solicitantes</h1>
+                </template>
+
+                <template v-if="menu==10">
+                    <h1>Reportes Registro de Expedientes</h1>
+                </template>
+
+                <template v-if="menu==11">
+                    <h1>Ayuda</h1>
+                </template>
+
+                <template v-if="menu==12">
+                    <h1>Acerca de</h1>
+                </template>
+         @elseif (Auth::user()->idrol == 2)
+             @include('plantilla.sidebarsecretaria')
+
+             <template v-if="menu==0">
+                <h1>Escritorio </h1>
+                </template>
+
+                <template v-if="menu==1">
+                    <oficina></oficina>
+                </template>
+
+                <template v-if="menu==2">
+                    <h1>Contenido del menú 2</h1>
+                </template>
+
+                <template v-if="menu==3">
+                    <expediente></expediente>
+                </template>
+
+                <template v-if="menu==4">
+                    <solicitante></solicitante>
+                </template>
+
+                <template v-if="menu==5">
+                    <regexpediente></regexpediente>
+                </template>        
+
+                <template v-if="menu==6">
+                    <h1>Contenido del menú 6</h1>
+                </template>
+                <template v-if="menu==13">
+                    <persona></persona>
+                </template>                
+
+                <template v-if="menu==9">
+                    <h1>Reporte Solicitantes</h1>
+                </template>
+
+                <template v-if="menu==10">
+                    <h1>Reportes Registro de Expedientes</h1>
+                </template>
+
+                <template v-if="menu==11">
+                    <h1>Ayuda</h1>
+                </template>
+
+                <template v-if="menu==12">
+                    <h1>Acerca de</h1>
+                </template>
+
+         @else
+           @endif
+
+          @endif
+
+
+
     @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,43 +11,98 @@
 |
 */
 
-Route::get('/', function () {
-    return view('contenido/contenido');
+Route::group(['middleware'=>['guest']],function(){
+    Route::get('/','Auth\LoginController@showLoginForm');
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
 });
 
-Route::get('/oficina','OficinaController@index');
-Route::post('/oficina/registrar','OficinaController@store');
-Route::put('/oficina/actualizar','OficinaController@update');
-Route::get('/oficina/selectOficina','OficinaController@selectOficina');
 
-Route::get('/expediente','ExpedienteController@index');
-Route::post('/expediente/registrar','ExpedienteController@store');
-Route::put('/expediente/actualizar','ExpedienteController@update');
-Route::get('/expediente/selectExpediente','ExpedienteController@selectExpediente');
+Route::group(['middleware'=>['auth']],function(){
 
+    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+    
 
-Route::get('/regexpediente','RegexpedienteController@index');
-Route::post('/regexpediente/registrar','RegexpedienteController@store');
-Route::put('/regexpediente/desactivar','RegexpedienteController@desactivar');
+    Route::get('/main', function () {
+        return view('contenido/contenido');
+    }) ->name('main');
+    
+    Route::group(['middleware'=>['Secretaria']],function(){
 
 
-Route::get('/persona','PersonaController@index');
-Route::post('/persona/registrar','PersonaController@store');
-Route::put('/persona/actualizar','PersonaController@update');
-Route::get('/persona/selectPersona','PersonaController@selectPersona');
+        Route::get('/oficina','OficinaController@index');
+        Route::post('/oficina/registrar','OficinaController@store');
+        Route::put('/oficina/actualizar','OficinaController@update');
+        Route::get('/oficina/selectOficina','OficinaController@selectOficina');
+        
+        Route::get('/expediente','ExpedienteController@index');
+        Route::post('/expediente/registrar','ExpedienteController@store');
+        Route::put('/expediente/actualizar','ExpedienteController@update');
+        Route::get('/expediente/selectExpediente','ExpedienteController@selectExpediente');
+
+        Route::get('/regexpediente','RegexpedienteController@index');
+        Route::post('/regexpediente/registrar','RegexpedienteController@store');
+        Route::put('/regexpediente/desactivar','RegexpedienteController@desactivar');
+
+        Route::get('/persona','PersonaController@index');
+        Route::post('/persona/registrar','PersonaController@store');
+        Route::put('/persona/actualizar','PersonaController@update');
+        Route::get('/persona/selectPersona','PersonaController@selectPersona');
+                
+        
+        Route::get('/solicitante', 'SolicitanteController@index');
+        Route::post('/solicitante/registrar', 'SolicitanteController@store');
+        Route::put('/solicitante/actualizar', 'SolicitanteController@update');
+        Route::get('/solicitante/selectSolicitante', 'SolicitanteController@selectSolicitante');
+
+    });
+     
+
+    Route::group(['middleware'=>['Administrador']],function(){
+
+        Route::get('/oficina','OficinaController@index');
+        Route::post('/oficina/registrar','OficinaController@store');
+        Route::put('/oficina/actualizar','OficinaController@update');
+        Route::get('/oficina/selectOficina','OficinaController@selectOficina');
+        
+        Route::get('/expediente','ExpedienteController@index');
+        Route::post('/expediente/registrar','ExpedienteController@store');
+        Route::put('/expediente/actualizar','ExpedienteController@update');
+        Route::get('/expediente/selectExpediente','ExpedienteController@selectExpediente');
+        
+        
+        Route::get('/regexpediente','RegexpedienteController@index');
+        Route::post('/regexpediente/registrar','RegexpedienteController@store');
+        Route::put('/regexpediente/desactivar','RegexpedienteController@desactivar');
+        
+        
+        Route::get('/persona','PersonaController@index');
+        Route::post('/persona/registrar','PersonaController@store');
+        Route::put('/persona/actualizar','PersonaController@update');
+        Route::get('/persona/selectPersona','PersonaController@selectPersona');
+        
+        
+        Route::get('/rol','RolController@index');
+        Route::get('/rol/selectRol','RolController@selectRol');
+        
+        Route::get('/user','UserController@index');
+        Route::post('/user/registrar','UserController@store');
+        Route::put('/user/actualizar','UserController@update');
+        Route::post('/user/desactivar','UserController@desactivar');
+        Route::put('/user/activar','UserController@activar');
+        
+        
+        Route::get('/solicitante', 'SolicitanteController@index');
+        Route::post('/solicitante/registrar', 'SolicitanteController@store');
+        Route::put('/solicitante/actualizar', 'SolicitanteController@update');
+        Route::get('/solicitante/selectSolicitante', 'SolicitanteController@selectSolicitante');
+        
 
 
-Route::get('/rol','RolController@index');
-Route::get('/rol/selectRol','RolController@selectRol');
-
-Route::get('/user','UserController@index');
-Route::post('/user/registrar','UserController@store');
-Route::put('/user/actualizar','UserController@update');
-Route::post('/user/desactivar','UserController@desactivar');
-Route::put('/user/activar','UserController@activar');
+    });
 
 
-Route::get('/solicitante', 'SolicitanteController@index');
-Route::post('/solicitante/registrar', 'SolicitanteController@store');
-Route::put('/solicitante/actualizar', 'SolicitanteController@update');
-Route::get('/solicitante/selectSolicitante', 'SolicitanteController@selectSolicitante');
+});
+
+
+
+//Route::get('/home', 'HomeController@index')->name('home');
